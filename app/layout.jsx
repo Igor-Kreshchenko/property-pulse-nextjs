edd570ed34b124f79ced7d0e65e4c354d9e44489
Igor_Kreshchenko <igor.kreshchenko@gmail.com>
2025-05-28 12:25:55 +0300

    add footer, hero, navbar and info boxes

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,5 @@
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 import '@/assets/styles/globals.css';
 
 export const metadata = {
@@ -11,7 +13,9 @@ const MainLayout = ({ children }) => {
   return (
     <html>
       <body>
+        <Navbar />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
---
+++ app/properties/[id]/page.jsx
@@ -0,0 +1,5 @@
+const PropertyPage = ({ params, searchParams }) => {
+  return <div>Property Page {searchParams.name}</div>;
+};
+
+export default PropertyPage;
