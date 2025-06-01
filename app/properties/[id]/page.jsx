9ee268219e461f7a48e928bf5532e18908e5e001
Igor_Kreshchenko <igor.kreshchenko@gmail.com>
2025-06-01 12:22:43 +0300

    add google auth

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,12 +1,14 @@
+import PropertyHeaderImage from '@/components/PropertyHeaderImage';
+import PropertyDetails from '@/components/PropertyDetails';
 import connectDB from '@/config/database';
 import Property from '@/models/Property';
-import PropertyHeaderImage from '@/components/PropertyHeaderImage';
 import Link from 'next/link';
 import { FaArrowLeft } from 'react-icons/fa';
 
 const PropertyPage = async ({ params }) => {
   await connectDB();
-  const property = await Property.findById(params.id).lean();
+  const { id } = await params;
+  const property = await Property.findById(id).lean();
 
   return (
     <>
@@ -24,7 +26,8 @@ const PropertyPage = async ({ params }) => {
       <section className="bg-blue-50">
         <div className="Namecontainer m-auto py-10 px-6">
           <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
-            Property Info
+            {/* Property Info */}
+            <PropertyDetails property={property} />
           </div>
         </div>
       </section>
