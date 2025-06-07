658c3782c759ec8c95ebb6b7edcc0564adbda3c4
Igor_Kreshchenko <igor.kreshchenko@gmail.com>
2025-06-07 12:22:35 +0300

    add property images

--- app/actions/addProperty.js
+++ app/actions/addProperty.js
@@ -4,6 +4,7 @@ import Property from '@/models/Property';
 import { getSessionUser } from '@/utils/getSessionUser';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import cloudinary from '@/config/cloudinary';
 
 async function addProperty(formData) {
   await connectDB();
@@ -14,10 +15,7 @@ async function addProperty(formData) {
   const { userId } = sessionUser;
 
   const amenities = formData.getAll('amenities');
-  const images = formData
-    .getAll('images')
-    .filter(image => image.name !== '')
-    .map(image => image.name);
+  const images = formData.getAll('images').filter(image => image.name !== '');
 
   const propertyData = {
     owner: userId,
@@ -44,8 +42,27 @@ async function addProperty(formData) {
       email: formData.get('seller_info.email'),
       phone: formData.get('seller_info.phone'),
     },
-    images,
   };
+
+  const imageUrls = [];
+
+  for (const image of images) {
+    const imageBuffer = await image.arrayBuffer();
+    const imageArray = Array.from(new Uint8Array(imageBuffer));
+    const imageData = Buffer.from(imageArray);
+
+    const imageBase64 = imageData.toString('base64');
+    const result = await cloudinary.uploader.upload(
+      `data:image/png;base64,${imageBase64}`,
+      {
+        folder: 'propertypulse',
+      },
+    );
+
+    imageUrls.push(result.secure_url);
+  }
+
+  propertyData.images = imageUrls;
 
   const newProperty = new Property(propertyData);
   await newProperty.save();
--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -2,6 +2,7 @@ import PropertyHeaderImage from '@/components/PropertyHeaderImage';
 import PropertyDetails from '@/components/PropertyDetails';
 import connectDB from '@/config/database';
 import Property from '@/models/Property';
+import PropertyImages from '@/components/PropertyImages';
 import Link from 'next/link';
 import { FaArrowLeft } from 'react-icons/fa';
 
@@ -31,6 +32,7 @@ const PropertyPage = async ({ params }) => {
           </div>
         </div>
       </section>
+      <PropertyImages images={property.images} />
     </>
   );
 };
