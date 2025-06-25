7f5a83d584b74208fc29d50d1a722783e7c21758
Igor_Kreshchenko <igor.kreshchenko@gmail.com>
2025-06-25 10:58:03 +0300

    ad featured properties

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,19 +5,19 @@ import { GlobalProvider } from '@/context/GlobalContext';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import '@/assets/styles/globals.css';
+import 'photoswipe/dist/photoswipe.css';
 
 export const metadata = {
-  title: 'Property Pulse',
-  keywords: 'property, real estate, listings, buy, sell, rent',
-  description:
-    'Property Pulse - Your go-to platform for real estate listings and insights.',
+  title: 'PropertyPulse',
+  description: 'Find The Perfect Rental Property',
+  keywords: 'rental, property, real estate',
 };
 
 const MainLayout = ({ children }) => {
   return (
     <AuthProvider>
       <GlobalProvider>
-        <html>
+        <html lang="en">
           <body>
             <Navbar />
             <main>{children}</main>
